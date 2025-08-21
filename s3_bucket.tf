ad0d9ecab5e06bae895e5e99200e42bc5a05247b
serfolaanu <folayemiemoruwa@gmail.com>
2025-08-21 08:34:18 +0100

    commit my s3_bucket.tf file to GitHub

---
+++ s3_bucket.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+    region = "eu-west-2"
+}
+
+#creation of the bucket
+resource "aws_s3_bucket" "public_bucket" {
+    bucket = "fola_jenkins_s3_bucket"
+}
+
+#object owner
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.public_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+#Public Accessibility to bucket enabled
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.public_bucket.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  depends_on = [
+    aws_s3_bucket_ownership_controls.ownership,
+    aws_s3_bucket_public_access_block.public_access,
+  ]
+  bucket = aws_s3_bucket.public_bucket.id
+  acl    = "public-read"
+}
